85cb8c38d5c3317b88319416b3b7293880cc784f
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 15:54:49 +0700

    fix: fix typo on blog.config

--- blog.config.js
+++ blog.config.js
@@ -29,7 +29,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twittee.com/fahdikrie',
+  socialLink: 'https://twitter.com/fahdikrie',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
