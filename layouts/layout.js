aa7b5f17df310112cd469ac76c1a9be57da670f1
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 15:53:09 +0700

    feat: change author's avatar

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
+                  src={'https://avatars.githubusercontent.com/u/47189456?v=4'}
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   webpack5: true,
   images: {
-    domains: ['gravatar.com']
+    domains: ['avatars.githubusercontent.com']
   },
   eslint: {
     dirs: [
