8691fbaa1a432142177cc72229d34b22fa2de228
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 23:51:43 +0700

    chore: add keyword

--- blog.config.js
+++ blog.config.js
@@ -31,7 +31,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/fahdikrie',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', 'Notion', 'fahdikrie'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
